4a4ae23b6d523506bb8aaec826861696609dbcab
Peachypakorn <pakorn.matana@gmail.com>
2016-05-20 19:23:38 +0700

    update landing
    add css html

---
+++ header.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link href="../css/bs/bootstrap.min.css" rel="stylesheet"> -->
+    <!-- // <script src="js/bs/bootstrap.min.js"></script> -->
+
+    <!-- CDN -->
+<!-- /bootstrap/bootstrap.min.css"> --> 
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<!-- <link rel="stylesheet" type="text/css" href="mystyle.css"> -->
+
+	</head>	
+</html>
---
+++ landing.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include 'header.php';?>
+<link href="header.php">
+<link rel="stylesheet" type="text/css" href="./css/landing.css">
+</head>
+
+<body>
+	<div>
+		<video autoplay loop poster="../img/landing.png" id="landbg">
+    	<!-- <source src="polina.webm" type="video/webm"> -->
+    	<source src="../img/video.mp4" type="video/mp4">
+		
+		</video>
+		<!-- <img class="absolute" src="../img/bg.gif" alt="touch"> -->
+		<img id="landinglogo" src="../img/unlessyoutry.png" alt="unless Logo">
+		<p>WELCOME TO THE COMMUNITY OF ENDLESS POSSIBLILITIES </p>
+	</div>	
+
+</body>
+</html>
