43f17bcb788812369367288a60777ce8e09d4d61
Peachypakorn <pakorn.matana@gmail.com>
2016-05-26 17:01:27 +0700

    add all pic in home

--- home.php
+++ home.php
@@ -82,11 +82,8 @@ $(document).ready(function(){
         height: '43%',
         width: '43%'
     });
-    	$(this).find(".text2").fadeIn(1200);
-    	// $(this).find(".text2").typed({
-     //    strings: ["First"," c"],
-     //    typeSpeed: 400
-     //  });
+    $(this).find("p.text2").fadeIn(700);
+    $(this).find("p.text3").fadeIn(700);
        }, function(){
 		$(this).find(".text").animate({
         left: '100%',
@@ -94,11 +91,8 @@ $(document).ready(function(){
         height: '43%',
         width: '43%'
     });
-		$(this).find(".text2").fadeOut(10);
-		// $(this).find(".text2").type({
-  //       strings: ["kdflkj"],
-  //       typeSpeed: 10
-  //     });
+		$(this).find("p.text2").hide();
+		$(this).find("p.text3").hide();
     });
 });
 </script>
@@ -145,66 +139,94 @@ $(document).ready(function(){
 		</div>
 			<div class="col-sm-8 check"  >
 					<div class="grow pic bw" id = "pic1">
-							<img  src="./img/homePic/Phum.jpg" alt="touch">
+							<img  src="./img/homePic/Phum.jpg" alt="phumConcert">
 							<div class = "text">
 							</div>
-							<p class = "text2" >PPP<br>PHUM<br>CONCERT</p>
+							<p class = "text2" >     <br>PHUM<br>CONCERT</p>
 							<p class = "text3" ><br><br><br>15.03.16</p>
 							
 						</div>
 					<div class="grow pic bw" id = "pic2">
+							<img  src="./img/homePic/Cindy.jpg" alt="Cindy">
+								<div class = "text">
+							</div>
+							<p class = "text2" >KOREA<br>ART<br>TRIP</p>
+							<p class = "text3" ><br><br><br>4-11.04.16</p>
+							
+
+						</div>						
+					<div class="grow pic bw" id = "pic3">
+							<img  src="./img/homePic/Bonus.jpg" alt="Bonus">
+								<div class = "text">
+							</div>
+							<p class = "text2" >HIGHER<br>LEVEL<br>DANCE</p>
+							<p class = "text3" ><br><br><br>18.02.16</p>
+							
+						</div>
+							<div class="grow pic bw" id = "pic4">
+							<img  src="./img/homePic/Mitz.jpg" alt="midjungle">
+								<div class = "text">
+							</div>
+							<p class = "text2" >WEIR<br>FOR<br>WILD</p>
+							<p class = "text3" ><br><br><br>28.02.16</p>
+						</div>
+					<div class="grow pic bw" id = "pic5">
 							<img  src="./img/homePic/Touch.jpg" alt="touch">
 								<div class = "text">
 							</div>
-							<p class = "text2" >TOUCH<br>BEING<br>GAY</p>
-							<p class = "text3" ><br><br><br>15.03.16</p>
-							
-
+							<p class = "text2" >RURAL<br>TEACHER<br>#170</p>
+							<p class = "text3" ><br><br><br>7-10.01.16</p>
 						</div>						
-					<div class="grow pic bw" id = "pic3">
-							<img  src="./img/homePic/Bonus.jpg" alt="touch">
-							<div class = "text">
-								hiiii
+					<div class="grow pic bw" id = "pic6">
+							<img  src="./img/homePic/Sun.jpg" alt="sunActing">
+								<div class = "text">
 							</div>
+							<p class = "text2" >THE<br>OUTER<br>PROJECT</p>
+							<p class = "text3" ><br><br><br>1-31.01.16</p>
 						</div>
-											<div class="grow pic bw" id = "pic1">
-							<img  src="./img/homePic/Phum.jpg" alt="touch">
-							<div class = "text">
-								hiiii
+					<div class="grow pic bw" id = "pic7">
+							<img  src="./img/homePic/PEP.jpg" alt="pep">
+								<div class = "text">
 							</div>
+							<p class = "text2" >KOH<br>TAO<br>DIVING</p>
+							<p class = "text3" ><br><br><br>17.05.16</p>
 						</div>
-					<div class="grow pic bw" id = "pic2">
-							<img  src="./img/homePic/Touch.jpg" alt="touch">
-							<div class = "text">
-								hiiii
+					<div class="grow pic bw" id = "pic8">
+							<img  src="./img/homePic/gino.jpg" alt="gino Russia">
+								<div class = "text">
 							</div>
+							<p class = "text2" >AFS<br>AT<br>RUSSIA</p>
+							<p class = "text3" ><br><br><br>2014-2015</p>
 						</div>						
-					<div class="grow pic bw" id = "pic3">
-							<img  src="./img/homePic/Bonus.jpg" alt="touch">
-							<div class = "text">
-								hiiii
+					<div class="grow pic bw" id = "pic9">
+							<img  src="./img/homePic/Dew.jpg" alt="dewdewdew">
+								<div class = "text">
 							</div>
+							<p class = "text2" >PHU<br>SOIDAOW<br>HIKING</p>
+							<p class = "text3" ><br><br><br>24-28.12.15</p>
 						</div>
-											<div class="grow pic bw" id = "pic1">
-							<img  src="./img/homePic/Phum.jpg" alt="touch">
-							<div class = "text">
-								hiiii
+					<div class="grow pic bw" id = "pic10">
+							<img  src="./img/homePic/Oak.jpg" alt="camping">
+								<div class = "text">
 							</div>
+							<p class = "text2" >AFS<br>MEDIA<br>MAN</p>
+							<p class = "text3" ><br><br><br>21-24.04.15</p>
 						</div>
-					<div class="grow pic bw" id = "pic2">
-							<img  src="./img/homePic/Touch.jpg" alt="touch">
-							<div class = "text">
-								hiiii
+					<div class="grow pic bw" id = "pic11">
+							<img  src="./img/homePic/Nat.jpg" alt="TFT">
+								<div class = "text">
 							</div>
-						</div>						
-					<div class="grow pic bw" id = "pic3">
-							<img  src="./img/homePic/Bonus.jpg" alt="touch">
-							<div class = "text">
-								hiiii
-							</div>
-						</div>
+							<p class = "text2" >TEACH<br>FOR<br>THAI</p>
+							<p class = "text3" ><br><br><br>21-24.04.15</p>
+						</div>	
 
 			</div>
+
+				<script>
+				$("p.text2").hide();
+				$("p.text3").hide();
+				</script>
+
 		</div>	
 	</div>
 </body>
