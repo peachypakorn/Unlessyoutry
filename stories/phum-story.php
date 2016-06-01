0b922b3497f3e59d21e3dd0c3108e5886fae047f
Peachypakorn <pakorn.matana@gmail.com>
2016-06-01 15:12:58 +0700

    ,,

--- stories/phum-story.php
+++ stories/phum-story.php
@@ -151,11 +151,11 @@ $(document).ready(function(){
           var vid = document.getElementById("story-vid");
           if (n > slides.length) {slideIndex = 1} 
           if (n < 1) {slideIndex = slides.length} ;
-          for (i = 0; i < slides.length; i++) {
-              slides[i].style.display = "none"; 
-          }
           for (i = 0; i < dots.length; i++) {
               dots[i].className = dots[i].className.replace(" active", "");
+          }
+          for (i = 0; i < slides.length; i++) {
+              slides[i].style.display = "none"; 
           }
           slides[slideIndex-1].style.display = "block";
           if(n!=2){
