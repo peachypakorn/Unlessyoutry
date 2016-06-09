6e9143cc22c5545e42994768dd86197ea561a45f
Peachypakorn <pakorn.matana@gmail.com>
2016-06-10 04:31:20 +0700

    check color

--- story_backup.php
+++ story_backup.php
@@ -163,7 +163,7 @@
 			<div class="grow pic bw slideUp " id = "pic1">
 				<img  src="./img/homePic/Phum.jpg" alt="phumConcert">
 				<div class = "text"></div>
-				<div class = "cata"></div>
+				<div class = "cata" style = "background:#1A9AAF" ></div>
 					<p class = "text2" >     <br>PHUM<br>CONCERT</p>
 					<p class = "text3" ><br><br><br>15.03.16</p>
 			</div>
@@ -172,7 +172,7 @@
 			<div class="grow pic bw slideUp" id = "pic2">
 				<img  src="./img/homePic/Cindy.jpg" alt="Cindy">
 				<div class = "text"></div>
-				<div class = "cata"></div>
+				<div class = "cata" style = "background:#1A9AAF" ></div>
 					<p class = "text2" >KOREA<br>ART<br>TRIP</p>
 					<p class = "text3" ><br><br><br>4-11.04.16</p>
 			</div>
@@ -181,7 +181,7 @@
 			<div class="grow pic bw slideUp" id = "pic3">
 				<img  src="./img/homePic/Bonus.jpg" alt="Bonus">
 				<div class = "text"></div>
-				<div class = "cata"></div>
+				<div class = "cata" style = "background:#1A9AAF" ></div>
 					<p class = "text2" >HIGHER<br>LEVEL<br>DANCE</p>
 					<p class = "text3" ><br><br><br>18.02.16</p>
 				</div>
@@ -190,7 +190,7 @@
 			<div class="grow pic bw slideUp" id = "pic4">
 				<img  src="./img/homePic/Mitz.jpg" alt="midjungle">
 				<div class = "text"></div>
-				<div class = "cata"></div>
+				<div class = "cata" style = "background:#4EBC90" ></div>
 					<p class = "text2" >WEIR<br>FOR<br>WILD</p>
 					<p class = "text3" ><br><br><br>28.02.16</p>
 			</div>
@@ -199,7 +199,7 @@
 			<div class="grow pic bw slideUp" id = "pic5">
 				<img  src="./img/homePic/Touch.jpg" alt="touch">
 				<div class = "text"></div>
-				<div class = "cata"></div>
+				<div class = "cata" style = "background:#4EBC90" ></div>
 					<p class = "text2" >RURAL<br>TEACHER<br>#170</p>
 					<p class = "text3" ><br><br><br>7-10.01.16</p>
 			</div>						
@@ -208,7 +208,7 @@
 			<div class="grow pic bw slideUp"  id = "pic6">
 				<img  src="./img/homePic/Sun.jpg" alt="sunActing">
 				<div class = "text"></div>
-				<div class = "cata"></div>
+				<div class = "cata" style = "background:#1A9AAF" ></div>
 					<p class = "text2" >THE<br>OUTER<br>PROJECT</p>
 					<p class = "text3" ><br><br><br>1-31.01.16</p>
 			</div>
@@ -217,7 +217,7 @@
 			<div class="grow pic bw slideUp" id = "pic7">
 				<img  src="./img/homePic/PEP.jpg" alt="pep">
 				<div class = "text"></div>
-				<div class = "cata"></div>
+				<div class = "cata" style = "background:#E45641" ></div>
 					<p class = "text2" >KOH<br>TAO<br>DIVING</p>
 					<p class = "text3" ><br><br><br>17.05.16</p>
 			</div>
@@ -226,7 +226,7 @@
 			<div class="grow pic bw slideUp" id = "pic8">
 				<img  src="./img/homePic/gino.jpg" alt="gino Russia">
 				<div class = "text"></div>
-				<div class = "cata"></div>
+				<div class = "cata" style = "background:#ECE4C2" ></div>
 					<p class = "text2" >AFS<br>AT<br>RUSSIA</p>
 					<p class = "text3" ><br><br><br>2014-2015</p>
 			</div>						
@@ -235,7 +235,7 @@
 			<div class="grow pic bw slideUp" id = "pic9">
 				<img  src="./img/homePic/Dew.jpg" alt="dewdewdew">
 				<div class = "text"></div>
-				<div class = "cata"></div>
+				<div class = "cata" style = "background:#F2AA4E" ></div>
 					<p class = "text2" >PHU<br>SOIDAOW<br>HIKING</p>
 					<p class = "text3" ><br><br><br>24-28.12.15</p>
 			</div>
@@ -244,7 +244,7 @@
 			<div class="grow pic bw slideUp" id = "pic10">
 				<img  src="./img/homePic/Oak.jpg" alt="camping">
 				<div class = "text"></div>
-				<div class = "cata"></div>
+				<div class = "cata" style = "background:#274763" ></div>
 					<p class = "text2" >AFS<br>MEDIA<br>MAN</p>
 					<p class = "text3" ><br><br><br>21-24.04.15</p>
 			</div>
@@ -253,7 +253,7 @@
 			<div class="grow pic bw slideUp" id = "pic11">
 				<img  src="./img/homePic/Nat.jpg" alt="TFT">
 				<div class = "text"></div>
-				<div class = "cata"></div>
+				<div class = "cata" style = "background:#ECE4C2" ></div>
 					<p class = "text2" >TEACH<br>FOR<br>THAI</p>
 					<p class = "text3" ><br><br><br>21-24.04.15</p>
 			</div>
